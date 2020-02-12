8c9b2b91dc40978d1e47b387699031a23ca0eb32
konstantin-poltavets <Kos@5240>
2020-02-12 11:26:53 +0200

    first

---
+++ bboard/setup.cmd
@@ -0,0 +1,9 @@
+pip install django
+pip install django-bootstrap4
+pip install social-auth-app-django
+pip install pillow
+pip install easy-thumbnails
+pip install django-cleanup
+pip install django-simple-captcha
+pip install djangorestframework
+pip install django-cors-headers
